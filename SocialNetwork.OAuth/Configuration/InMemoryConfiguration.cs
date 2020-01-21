98cc1749dca8c203a7c64e73b553579cd6322c40
dortiz <dortiz@bitworks.com.sv>
2020-01-21 17:33:09 -0600

    Avance en el API

---
+++ SocialNetwork.OAuth/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace SocialNetwork.OAuth.Configuration
+{
+    /// <summary>
+    /// Configuration for the InMemory model
+    /// </summary>
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("socialnetwork", "Social Network")
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+                new Client()
+                {
+                    ClientId = "socialnetwork",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials
+                }
+            };
+        }
+    }
+}
